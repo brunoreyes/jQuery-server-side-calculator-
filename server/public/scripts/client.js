9bb9de9774fadf971c895ec9b15ce290a647ae30
Bruno Reyes <brunoreyes@MacBook-Pro.local>
2020-06-28 22:11:04 -0500

    debugged function addEvent listener, got numbers to display in calculator

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,27 +1,16 @@
-$(document).readyState(onReady);
 
-// 
+$(document).ready(onReady);
+
 let displayedOperator;
-
 let operator;
-
 let firstNumber = '';
 let secondNumber = '';
 
 function onReady() {
   console.log("JQ");
   addEventListeners();
-} //end onReady
 
-
-//this function replaces old results with new results
-function updateCalculatedResult() {
-  console.log("in updateCalculatedResult");
-  // Here I am emptying out the Results area
-  $("#calculationResult").empty();
-  // Here I am appending the new results
-  $("#calculationResult").append(newResult);
-}// end updateCalculatedResults
+} //end onReady()
 
 
 
@@ -33,10 +22,8 @@ function addCalculation(calculatorInput) {
   console.log('in addCalculation');
   // I want to prevent the page from reloading
   // to keep my calculation history growing
-  let calculatorInput = $('#calculatorInput').val();
-
+  calculatorInput = $('#calculatorInput').val();
   console.log("sending:", calculatorInput);
-
   $.ajax({
     //POST is a two-way street, GET is a one-way street
     type: 'POST',
@@ -45,33 +32,29 @@ function addCalculation(calculatorInput) {
   })
     .then(function (response) {
       console.log("back from server with:", response);
-      showCalculation(response);
-
+      updateCalculatedResult(response);
+      addCalculationToHistory(response)
     })
     .catch(function (err) {
       alert("error calculating recent input. see console for details");
       console.log(err);
     });
-} // end addCalculation
-
-
-
-
+} // end addCalculation()
 
 function addEventListeners() {
-
+  console.log('in addEventListeners');
+  
   // '.operator'
-  $('.operator').on('click', 'button', function() {
+  $('#numberContainer').on('click', '.operator', function () {
+    console.log('in .operator click handler function');
     if (firstNumber === '') { // if firstNumber hasn't been entered
       alert('Please enter a number before using an operator.');
-
     } else if (secondNumber === '') { // if firstNumber was entered but not secondnumber
       // assign operator/displayedOperator variables based on the button that was clicked
-      setOperation(this.name, $(this).text());
-
+      operatorCall(this.name, $(this).text());
       // update calculatorDisplay with selected numOne & operator
       $('#calculatorInput').empty();
-      $('#calculatorInput').append(`${ firstNumber } ${ displayedOperator }`);
+      $('#calculatorInput').val(`${ firstNumber } ${ displayedOperator }`);
     } else { // if both numOne && numTwo have been entered & operator is assigned:
       // store inputted numbers & operators into an object
       let calculation = {
@@ -79,93 +62,114 @@ function addEventListeners() {
         secondNumber: secondNumber,
         operator: operator
       };//end else
-
     // send calculation to the server via addCalculation() function
     addCalculation(calculation);
       // assign operator/displayedOperator variables based on the button that was clicked
       operatorCall(this.name, $(this).text());
     } // end else 
-  }); // end '.operator'
+  }); // end '.operator' click handler function
   
   // '#equal'
-  $('#equal').on('submit', function (calculating) {
+  $('#numberContainer').on('submit', '#equal', function (calculating) {
+    console.log('in #equal click handler function');
     // prevent page auto refreshing
     calculating.preventDefault();
     // store numbers & operators entered into
     // calculator into an object
-    var calculation = {
-      numOne: numOne,
-      numTwo: numTwo,
+    let calculation = {
+      firstNumber: firstNumber,
+      secondNumber: secondNumber,
       operator: operator
     };
     // sending calculation to server via addCalculation()
     addCalculation(calculation);
     addCalculationToHistory(response)
-  }); // end '#equal'
+  }); // end '#equal' click handler function
 
   //'#clear'
-  $('#clear').on('click', function() {
+  $('#clear').on('click', function () {
+    console.log('in #clear click handler function');
+    // preventDefault
     // reset numOne, numTwo, operator and displayedOperator variables
-    numOne = '';
-    numTwo = '';
+    firstNumber = '';
+    secondNumber = '';
     operator = undefined;
     displayedOperator = undefined;
     // empty both number inputs and any results that are displayed on the DOM
     $('#calculationResult').empty();
     $('#calculatorInput').empty();
-  }); // end '#clear'  
+  }); // end '#clear' click handler function  
 
   //'.number'
-  $('.numbers').on('click', 'button', function () {
+  $('#numberContainer').on('click', '.number', function () {
+    console.log('.number click handler function');
     // if only the 1st # is pressed bc opeator isn't defined bc isn't pressed
     // then only display first number
     if (operator === undefined) {
       // show only the firstNumber
+      console.log(this);
+      
       firstNumber += $(this).text();
       console.log('firstNumber', firstNumber);
       $('#calculatorInput').empty();
-      $('#calculatorInput').append(`${ firstNumber }`);
+      $('#calculatorInput').val(`${firstNumber}`);
+      // we can't append to an input field
     } else {
       // show the whole calculation
       secondNumber += $(this).text();
       $('#calculatorInput').empty();
-      $('#calculatorInput').append(`${firstNumber} ${displayedOperator} ${secondNumber}`);
+      $('#calculatorInput').val(`${firstNumber} ${displayedOperator} ${secondNumber}`);
       console.log('secondNumber', secondNumber);
     } // end else
-  }); // end '.number'
+  }); // end '.number' click handler function
 
-$('#decimal').on('click', 'button', function() {
-  event.preventDefault();
-  let currentDisplay = $('#calculatorInput').val();
-  // only if currentDisplay doesn't have a decimal at the end of it, add one
-  if (currentDisplay.slice(-1) != '.') {
-    $('#calculatorInput').val(currentDisplay + '.');
-  } // end if
-}// end '#decimal'
+    //'#decimal'
+  // $('#numberContainer').on('click', 'button', '#decimal', decimalFunction);
+
 } // end addEventListeners()
 
-// setter for operator & displayedOperator
+function decimalFunction(){
+    console.log('in #decimal click handler function');
+    event.preventDefault();
+    let currentDisplay = $('#calculatorInput').val();
+    // only if currentDisplay doesn't have a decimal at the end of it, add one
+    if (currentDisplay.slice(-1) != '.') {
+      $('#calculatorInput').val(currentDisplay + '.');
+    } // end if
+  };// end decimalFunction click handler function
+
+// sets the operator & displayedOperator
 function operatorCall(akaOperator, realOperator) {
+  console.log('in operatorCall');
   operator = akaOperator;
   displayedOperator = realOperator;
 }//end operatorCall
 
-// setter for firstNumber
+// sets the firstNumber
 function setFirstNumber(number) {
+  console.log('in setFirstNumber');
   firstNumber = number;
 }// end setFirstNumber
 
-// setter for secondNumber
+// sets the secondNumber
 function setSecondNumber(number) {
+  console.log('in setSecondNumber');
   secondNumber = number;
 }// end setSecondNumber
 
+//this function updates results
+function updateCalculatedResult(response) {
+  console.log("in updateCalculatedResult");
+  // Here I am emptying out the Results area
+  $("#calculationResult").empty();
+  // Here I am appending the new results
+  $("#calculationResult").append(response);
+}// end updateCalculatedResults
 
 function addCalculationToHistory(response) {
   console.log("in showCalculation", response);
 
     $("#calculatedHistory").append(`<li>${response}</li>`);
-  }
-} //end addCalculationToHistory()
+  } //end addCalculationToHistory()
 
 
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
+
 //USES
 // This must be added before GET & POST routes 
 // & is another requirement of bodyParser
@@ -17,3 +18,55 @@ app.use(express.static("server/public"));
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
 });
+
+
+// GET & POST Routes go here
+app.post( '/calculation', ( req, res )=>{
+  console.log('in /calculation GET');
+
+  // The data arrives here as "req.body", which is like an envelop that has alot of nonsense in it
+  console.log(req.body);
+
+  // here we are making it easier by setting a variable = req.body
+  // therefore using the variable instead of req.body
+  let calculation = req.body
+
+  //here we are sending the data to a function
+  let result = equalizer(calculation).toString();
+
+  // res.send takes whatever variable we have and sends it back to the client in this case it is 
+  res.send( result );
+
+  // res.sendStatus sends back an HTTP status code 201: something was created with the request that was received
+  // 200: everything went well 
+  // res.sendStatus(201); //not sure if it's code 201 or 200
+// })
+
+// equalizer() calculates based on a switcher 
+function equalizer(calculation) {
+  let x = parseFloat(calculation.firstNumber);
+  let y = parseFloat(calculation.secondNumber);
+  let operation = calculation.operator;
+  let result;
+
+  // parseFloat will start at the beginning
+  // but it has to start with a # or space but no letters
+
+  switch (operation) {
+    case 'add':
+      result = x + y;
+      break;
+    case 'subtract':
+      result = x - y;
+      break;
+    case 'multiply':
+      result = x * y;
+      break;
+    case 'divide':
+      result = x / y;
+      break;
+    default:
+      result = "calculator error";
+  }
+  return result;
+}
